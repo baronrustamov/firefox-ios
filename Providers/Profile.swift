5978949f13e612f96adc3b3b0e57ffb22c1a86cf
Tarik Eshaq <teshaq@mozilla.com>
2022-11-16 15:05:54 -0500

    Removes configuration for places history and enables it by default

--- Providers/Profile.swift
+++ Providers/Profile.swift
@@ -39,17 +39,6 @@ public protocol SyncManager {
 
 typealias SyncFunction = (SyncDelegate, Prefs, Ready, SyncReason) -> SyncResult
 
-public enum HistoryMigrationConfiguration {
-    case disabled
-    case dryRun
-    case real
-}
-
-public enum HistoryAPIConfiguration {
-    case old
-    case new
-}
-
 class ProfileFileAccessor: FileAccessor {
     convenience init(profile: Profile) {
         self.init(localName: profile.localName())
@@ -102,7 +91,6 @@ protocol Profile: AnyObject {
     var logins: RustLogins { get }
     var certStore: CertStore { get }
     var recentlyClosedTabs: ClosedTabsStore { get }
-    var historyApiConfiguration: HistoryAPIConfiguration { get }
 
     #if !MOZ_TARGET_NOTIFICATIONSERVICE
         var readingList: ReadingList { get }
@@ -228,8 +216,6 @@ open class BrowserProfile: Profile {
 
     var syncDelegate: SyncDelegate?
 
-    var historyApiConfiguration: HistoryAPIConfiguration
-
     /**
      * N.B., BrowserProfile is used from our extensions, often via a pattern like
      *
@@ -242,17 +228,12 @@ open class BrowserProfile: Profile {
      * A SyncDelegate can be provided in this initializer, or once the profile is initialized.
      * However, if we provide it here, it's assumed that we're initializing it from the application.
      */
-    init(localName: String, syncDelegate: SyncDelegate? = nil, clear: Bool = false, isNewHistoryPlacesAPI: Bool = false) {
+    init(localName: String, syncDelegate: SyncDelegate? = nil, clear: Bool = false) {
         log.debug("Initing profile \(localName) on thread \(Thread.current).")
         self.name = localName
         self.files = ProfileFileAccessor(localName: localName)
         self.keychain = MZKeychainWrapper.sharedClientAppContainerKeychain
         self.syncDelegate = syncDelegate
-        if isNewHistoryPlacesAPI {
-            self.historyApiConfiguration = .new
-        } else {
-            self.historyApiConfiguration = .old
-        }
 
         if clear {
             do {
@@ -379,20 +360,17 @@ open class BrowserProfile: Profile {
                 let visit = SiteVisit(site: site,
                                       date: Date().toMicrosecondsSince1970(),
                                       type: visitType)
-                history.addLocalVisit(visit)
-                if self.historyApiConfiguration == .new {
-                    let result = self.places.applyObservation(
-                        visitObservation: VisitObservation(
-                            url: url.description,
-                            title: title as String,
-                            visitType: VisitTransition.fromVisitType(visitType: visitType)
-                        )
+                let result = self.places.applyObservation(
+                    visitObservation: VisitObservation(
+                        url: url.description,
+                        title: title as String,
+                        visitType: VisitTransition.fromVisitType(visitType: visitType)
                     )
-                    result.upon { result in
-                        guard result.isSuccess else {
-                            SentryIntegration.shared.sendWithStacktrace(message: result.failureValue?.localizedDescription ?? "Unknown error adding history visit", tag: .rustPlaces, severity: .error)
-                            return
-                        }
+                )
+                result.upon { result in
+                    guard result.isSuccess else {
+                        SentryIntegration.shared.sendWithStacktrace(message: result.failureValue?.localizedDescription ?? "Unknown error adding history visit", tag: .rustPlaces, severity: .error)
+                        return
                     }
                 }
             }
@@ -465,34 +443,14 @@ open class BrowserProfile: Profile {
     lazy var browserDbPath =  URL(fileURLWithPath: (try! self.files.getAndEnsureDirectory())).appendingPathComponent("browser.db").path
     lazy var places: RustPlaces = RustPlaces(databasePath: self.placesDbPath)
 
-    public func migrateHistoryToPlaces(migrationConfig: HistoryMigrationConfiguration, callback: @escaping (HistoryMigrationResult) -> Void, errCallback: @escaping (Error?) -> Void) {
+    public func migrateHistoryToPlaces(callback: @escaping (HistoryMigrationResult) -> Void, errCallback: @escaping (Error?) -> Void) {
         let lastSyncTimestamp = Int64(self.syncManager.lastSyncFinishTime ?? 0)
-        switch migrationConfig {
-        case .disabled:
-            break
-        case .dryRun:
-            let dryRunPath = URL(fileURLWithPath: (try! self.files.getAndEnsureDirectory())).appendingPathComponent("dry-run-places.db").path
-            RustPlaces(databasePath: dryRunPath)
-                .migrateHistory(
-                    dbPath: self.browserDbPath,
-                    lastSyncTimestamp: lastSyncTimestamp,
-                    completion: { result in
-                    do {
-                        try FileManager.default.removeItem(atPath: dryRunPath)
-                    } catch {
-                        log.error("Unable do delete dry run database")
-                    }
-                    callback(result)
-                },
-                errCallback: errCallback)
-        case .real:
-            self.places.migrateHistory(
-                dbPath: self.browserDbPath,
-                lastSyncTimestamp: lastSyncTimestamp,
-                completion: callback,
-                errCallback: errCallback
-            )
-        }
+        self.places.migrateHistory(
+            dbPath: self.browserDbPath,
+            lastSyncTimestamp: lastSyncTimestamp,
+            completion: callback,
+            errCallback: errCallback
+        )
     }
 
     lazy var tabsDbPath = URL(fileURLWithPath: (try! files.getAndEnsureDirectory()), isDirectory: true).appendingPathComponent("tabs.db").path
@@ -965,12 +923,7 @@ open class BrowserProfile: Profile {
                 return self.profile.tabs.resetSync() >>> { ClientsSynchronizer.resetClientsWithStorage(self.profile.remoteClientsAndTabs, basePrefs: self.prefsForSync) }
 
             case "history":
-                switch self.profile.historyApiConfiguration {
-                case .old:
-                    return HistorySynchronizer.resetSynchronizerWithStorage(self.profile.history, basePrefs: self.prefsForSync, collection: "history")
-                case .new:
-                    return self.profile.places.resetHistoryMetadata()
-                }
+                return self.profile.places.resetHistoryMetadata()
             case "passwords":
                 return self.profile.logins.resetSync()
             case "forms":
@@ -997,14 +950,8 @@ open class BrowserProfile: Profile {
                 profile.remoteClientsAndTabs.onRemovedAccount,
                 profile.logins.resetSync,
                 profile.places.resetBookmarksMetadata,
+                profile.places.resetHistoryMetadata,
             ]
-            switch profile.historyApiConfiguration {
-            case .old:
-                remove.append(profile.history.onRemovedAccount)
-            case .new:
-                remove.append(profile.places.resetHistoryMetadata)
-            }
-
             let clearPrefs: () -> Success = {
                 withExtendedLifetime(self) {
                     // Clear prefs after we're done clearing everything else -- just in case
@@ -1078,12 +1025,6 @@ open class BrowserProfile: Profile {
                 accountManager.deviceConstellation()?.refreshState()
                 return deferMaybe(result)
             }
-        }
-
-        fileprivate func syncLegacyHistoryWithDelegate(_ delegate: SyncDelegate, prefs: Prefs, ready: Ready, why: SyncReason) -> SyncResult {
-            log.debug("Syncing history to storage.")
-            let historySynchronizer = ready.synchronizer(HistorySynchronizer.self, delegate: delegate, prefs: prefs, why: why)
-            return historySynchronizer.synchronizeLocalHistory(self.profile.history, withServer: ready.client, info: ready.info, greenLight: self.greenLight())
         }
 
         public class ScopedKeyError: MaybeErrorType {
@@ -1414,16 +1355,11 @@ open class BrowserProfile: Profile {
                 return Success()
             }
 
-            var historySynchronizer = self.syncLegacyHistoryWithDelegate
-            if self.profile.historyApiConfiguration == .new {
-                historySynchronizer = self.syncHistoryWithDelegate
-            }
-
             let synchronizers = [
                 ("clients", self.syncClientsWithDelegate),
                 ("tabs", self.syncTabsWithDelegate),
                 ("bookmarks", self.syncBookmarksWithDelegate),
-                ("history", historySynchronizer),
+                ("history", self.syncHistoryWithDelegate),
                 ("logins", self.syncLoginsWithDelegate)
             ]
 
@@ -1464,13 +1400,7 @@ open class BrowserProfile: Profile {
                 case "tabs": return ("tabs", self.syncTabsWithDelegate)
                 case "logins": return ("logins", self.syncLoginsWithDelegate)
                 case "bookmarks": return ("bookmarks", self.syncBookmarksWithDelegate)
-                case "history":
-                    switch self.profile.historyApiConfiguration {
-                    case .old:
-                        return ("history", self.syncLegacyHistoryWithDelegate)
-                    case .new:
-                        return ("history", self.syncHistoryWithDelegate)
-                    }
+                case "history": return ("history", self.syncHistoryWithDelegate)
                 default: return nil
                 }
             }
@@ -1483,7 +1413,7 @@ open class BrowserProfile: Profile {
         }
 
         public func hasSyncedHistory() -> Deferred<Maybe<Bool>> {
-            return self.profile.history.hasSyncedHistory()
+            return self.profile.hasSyncedLogins()
         }
 
         public func syncClients() -> SyncResult {
@@ -1511,12 +1441,7 @@ open class BrowserProfile: Profile {
         }
 
         public func syncHistory() -> SyncResult {
-            switch self.profile.historyApiConfiguration {
-            case .old:
-                return self.sync("history", function: syncLegacyHistoryWithDelegate)
-            case .new:
-                return self.sync("history", function: syncHistoryWithDelegate)
-            }
+            return self.sync("history", function: syncHistoryWithDelegate)
         }
 
         /**
